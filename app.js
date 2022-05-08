b528377eaa68d596595da5f627348332e52a0b0d
MarkMastro <mastropaolo.mark@gmail.com>
2022-05-07 20:20:37 -0400

    completed api weather call

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
+import 'dotenv/config'
 import express from 'express';
+import axios from 'axios';
+
+const port = 3000;
+const API_KEY = process.env.API_KEY;
 
 const app = express();
 
+// const callApi = async (location)=>{
+//   try{
+//     const {data} = await axios.get(`https://api.worldweatheronline.com/premium/v1/weather.ashx?key=${API_KEY}&q=${location}&format=json`)
+//     return data.data.current_condition[0].temp_C
+//     } catch(error) {
+//       console.log("error is ", error)
+//   }
+// }
+
 app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
+  try{
+    axios.get(`https://api.worldweatheronline.com/premium/v1/weather.ashx?key=${API_KEY}&q=${req.query.location}&format=json`)
+    .then(response=> res.send(response.data.data.current_condition[0]))
+    
+    } catch(error) {
+      console.log("error is ", error)
+  }
 });
 
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
-});
 
-app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
-});
-
-app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
-});
-
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`),
 );
